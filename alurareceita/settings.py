7d22beec4f590392d9110751fa449045145f44b7
marcos7junior <marcospaivajr7@gmail.com>
2022-09-24 11:19:27 -0300

    alterando settings

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'alura_receita',
         'USER': 'postgres',
-        'PASSWORD': 'debora7',
+        'PASSWORD': '******7',
         'HOST': 'localhost'
     }
     # 'default': {
